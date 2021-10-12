a37251bf8d80da18162166cf86af559a71abf9b2
adit450 <vaswani.a@husky.neu.edu>
2021-10-12 08:07:36 -0400

    Test

--- js/main.js
+++ js/main.js
@@ -19,8 +19,52 @@ let margin = {
 let svg1 = d3.select('#d3-container')
   .append('svg')
   .attr('preserveAspectRatio', 'xMidYMid meet') // this will scale your visualization according to the size of its parent element and the page.
-  .attr('width', '60%') // this is now required by Chrome to ensure the SVG shows up at all
-  .style('background-color', 'white') 
+  .attr('width', '100%') // this is now required by Chrome to ensure the SVG shows up at all
+  .style('background-color', 'blue')
   .style('border', 'solid')
   .attr('viewBox', [0, 0, width + margin.left + margin.right, height + margin.top + margin.bottom].join(' '))
 
+
+
+
+//https://www.tutorialsteacher.com/d3js/create-bar-chart-using-d3js
+/*
+var svg = d3.select("svg"),
+    margin = 200,
+    width = svg.attr("width") - margin,
+    height = svg.attr("height") - margin
+
+
+var xScale = d3.scaleBand().range([0, width]).padding(0.4),
+    yScale = d3.scaleLinear().range([height, 0]);
+
+var g = svg.append("g")
+    .attr("transform", "translate(" + 100 + "," + 100 + ")");
+
+d3.csv("XYZ.csv", function(error, data) {
+    if (error) {
+        throw error;
+    }
+
+    xScale.domain(data.map(function(d) { return d.year; }));
+    yScale.domain([0, d3.max(data, function(d) { return d.value; })]);
+
+    g.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(xScale));
+
+    g.append("g")
+        .call(d3.axisLeft(yScale).tickFormat(function(d){
+            return "$" + d;
+        }).ticks(10));
+
+
+    g.selectAll(".bar")
+        .data(data)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d) { return xScale(d.year); })
+        .attr("y", function(d) { return yScale(d.value); })
+        .attr("width", xScale.bandwidth())
+        .attr("height", function(d) { return height - yScale(d.value); })});
+*/
