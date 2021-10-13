f069f522736a3ba89f7d1179df54371e51f9823a
adit450 <vaswani.a@husky.neu.edu>
2021-10-13 13:23:13 -0400

    Adding Tooltip 4

--- js/main.js
+++ js/main.js
@@ -59,7 +59,7 @@ d3.csv("data/data.csv").then( function (data) {
         .on("mouseover", function(d){
             d3.select(this)
                 .attr('fill','blue')
-                .text(d.X)
+                .text(this.x)
                 .style("visibility", "visible");})
 
     //Append the x-axis to the SVG
